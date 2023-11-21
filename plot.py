2bf366c4980442a279ebb797387978094f11221f
JuandeJuni <juni55mcyt@gmail.com>
2023-11-21 13:23:02 +0100

    Add plot of spectrum

--- plot.py
+++ plot.py
@@ -5,22 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plotSpectogram(path):
-    y, sr = librosa.load(path)
-    spec = np.fft.fft(y)
-    # shifted_spec=np.fft.fftshift(spec)
-    freq = np.fft.fftfreq(y.shape[-1])
-    D = librosa.stft(y)
-    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
-    # fig, (ax1,ax2) = plt.subplots(2)
-    plt.plot(freq, spec.real, freq, spec.imag)
-    
-    
-    # plt.magnitude_spectrum(y)
-    # img = librosa.display.specshow(S_db, x_axis='time', y_axis='log', ax=ax2)
-    # ax2.set(title='Using a logarithmic frequency axis')
-    # fig.colorbar(img, ax=ax2, format="%+2.f dB")
-    
+def plotSpectogram(y,fs=44100):
+    fig, (ax1,ax2,ax3) = plt.subplots(3,figsize=(10,5),layout='constrained')
+    ax1.plot(y)
+    ax2.magnitude_spectrum(y,Fs=fs,c="r")
+    ax3.magnitude_spectrum(y,Fs=fs,scale="dB",c="g")
+    ax1.set_title("Signal")
+
     
     plt.show()
-plotSpectogram("sounds/room_service_bass.m4a")
